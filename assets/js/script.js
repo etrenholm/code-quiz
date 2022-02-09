76ce44672dd4d846acec8d8edc397de9d2c6aaed
Erica <erica.trenholm48@gmail.com>
2022-02-09 17:34:34 -0500

    created html and pseudocode in javascript files

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// When button is clicked, start timer and present first question
+// When question is answered incorrectly, alert that question was wrong and subtract from the clock
+// When a question is answered, alert that question was right and present another question
+// When all questions are answered OR the timer reaches 0, game is over
+// When the game is over, save initials and score in local storage
+// When page is reoloaded, old score reloads from local storage
