cfd22d81d5136f1becea2a44724d67a425c14fbd
Erica <erica.trenholm48@gmail.com>
2022-02-10 21:47:35 -0500

    added timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,85 +1,116 @@
 // When button is clicked, start timer and present first question
 var startBtn = document.querySelector("#start");
 startBtn.addEventListener("click", startQuiz);
+// startBtn.addEventListener("click", startTimer);
 
 var introContainerEl = document.querySelector("#intro-container");
+var mainContainerEl = document.querySelector("#main-container");
 var questionContainerEl = document.querySelector("#question-container");
 var endContainerEl = document.querySelector("#end-container");
+var timerEl = document.querySelector("#timer");
+
+mainContainerEl.setAttribute("style", "display:none;"); // hide quiz page
+endContainerEl.setAttribute("style", "display:none;"); // hide end page
 
 var quizQuestions = [
     {
-        id: 1,
-        question: "This is Question 1?",
-        answer: "a. this is a",
+        question: "What operator is used to assign a value to a declared variable? ",
+        answer: "c. Equal sign (=)",
         possibleAnswers: [
-            "a. this is a",
-            "b. this is b",
-            "c. this is c",
-            "d. this is d"
+            "a. Question mark (?)",
+            "b. Colon (:)",
+            "c. Equal sign (=)",
+            "d. Double-equal (==)"
         ],
     },
     {
-        id: 2,
-        question: "This is Question 2?",
-        answer: "a. this is a",
+        question: "How do we declare a conditional statement in JavaScript?",
+        answer: "c. for loop",
         possibleAnswers: [
-            "a. this is a",
-            "b. this is b",
-            "c. this is c",
-            "d. this is d"
-        ],
-    },
-    {
-        id: 3,
-        question: "This is Question 3",
-        answer: "a. this is a",
-        possibleAnswers: [
-            "a. this is a",
-            "b. this is b",
-            "c. this is c",
-            "d. this is d"
+            "a. while loop",
+            "b. difference...between",
+            "c. for loop",
+            "d. if...else"
         ],
     },
 ];
 
 // When button is clicked, start the quiz
 function startQuiz() {
-    console.log('started');
+    
+    introContainerEl.setAttribute("style", "display:none;"); // hide start page
+    mainContainerEl.setAttribute("style", "display:block;"); // display quiz page
+    startTimer();
     startQuestions();
 
 }
-// startQuiz();
 
 // When button is clicked, start the timer
-
-// var questionEl = document.querySelector("#question");
-var questionEl = document.querySelector("#question");
-questionEl.innerHTML = "Test";
-
-var answerButtonEl1 = document.querySelector("#option1");
-answerButtonEl1.innerHTML = "test";
-
-var answerButtonEl2 = document.querySelector("#option2");
-answerButtonEl2.innerHTML = "test2";
-
-var answerButtonEl3 = document.querySelector("#option3");
-answerButtonEl3.innerHTML = "test3";
-
-var answerButtonEl4 = document.querySelector("#option4");
-answerButtonEl4.innerHTML = "test4";
+function startTimer () {
+    var timeLeft = 49; 
+    var timeInterval = setInterval(function() {
+        if(timeLeft>0) {
+            timerEl.innerHTML = "Time Left: " + timeLeft;
+            timeLeft--
+        }
+        else {
+            clearInterval(timeInterval);
+            timerEl.innerHTML = "Time Left: 0"
+        }
+        console.log(timeLeft)
+    }, 1000)
+};
 
 
 function startQuestions() {
+
+    var questionEl = document.querySelector("#question");
+    questionEl.innerHTML = quizQuestions[0].question;
+
+    var answerButtonEl1 = document.querySelector("#option1");
+    answerButtonEl1.textContent = quizQuestions[0].possibleAnswers[0];
+
+    var answerButtonEl2 = document.querySelector("#option2");
+    answerButtonEl2.textContent  = quizQuestions[0].possibleAnswers[1];
+
+    var answerButtonEl3 = document.querySelector("#option3");
+    answerButtonEl3.textContent  = quizQuestions[0].possibleAnswers[2];
+
+    var answerButtonEl4 = document.querySelector("#option4");
+    answerButtonEl4.textContent = quizQuestions[0].possibleAnswers[3];
+
+    var answerContainer = document.querySelector("#answer-buttons");
+    answerContainer.addEventListener("click", nextQuestion);
 }
+
+function nextQuestion() {
+
+}
+
+
+// for(var i = 0; i < quizQuestions.length; i++)
+
+// When question is answered incorrectly, alert that question was wrong
+// When question is answered incorrectly, subtract from the clock
+// When question is answered correctly, alert that question was right
+// When a question is answered, present another question
 
 function selectAnswer() {
+    if(givenAnswer === correctAnswer) {
+
+    }
 }
 
-// When question is answered incorrectly, alert that question was wrong and subtract from the clock
-
-// When a question is answered, alert that question was right and present another question
-
 // When all questions are answered OR the timer reaches 0, game is over
+
+// After last question, go to end screen
+function endScreen() {
+
+    introContainerEl.setAttribute("style", "display:none;"); // hide start page
+    mainContainerEl.setAttribute("style", "display:none;"); // hide quiz page
+    endContainerEl.setAttribute("style", "display:block;"); // show end page
+
+}
 
 // When the game is over, save initials and score in local storage
 
