a656cd8172ed8aa6a7580416a6f3845832406409
Erica <erica.trenholm48@gmail.com>
2022-02-11 20:58:02 -0500

    edited questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,17 @@
 // When button is clicked, start timer and present first question
 var startBtn = document.querySelector("#start");
 startBtn.addEventListener("click", startQuiz);
-// startBtn.addEventListener("click", startTimer);
 
 var introContainerEl = document.querySelector("#intro-container");
 var mainContainerEl = document.querySelector("#main-container");
 var questionContainerEl = document.querySelector("#question-container");
 var endContainerEl = document.querySelector("#end-container");
 var timerEl = document.querySelector("#timer");
+var resultEl = document.querySelector("#result")
+var answerButtonEl = document.querySelector("#answer-buttons")
 
 mainContainerEl.setAttribute("style", "display:none;"); // hide quiz page
 endContainerEl.setAttribute("style", "display:none;"); // hide end page
-
-var quizQuestions = [
-    {
-        question: "What operator is used to assign a value to a declared variable? ",
-        answer: "c. Equal sign (=)",
-        possibleAnswers: [
-            "a. Question mark (?)",
-            "b. Colon (:)",
-            "c. Equal sign (=)",
-            "d. Double-equal (==)"
-        ],
-    },
-    {
-        question: "How do we declare a conditional statement in JavaScript?",
-        answer: "c. for loop",
-        possibleAnswers: [
-            "a. while loop",
-            "b. difference...between",
-            "c. for loop",
-            "d. if...else"
-        ],
-    },
-];
-
-// When button is clicked, start the quiz
-function startQuiz() {
-    
-    introContainerEl.setAttribute("style", "display:none;"); // hide start page
-    mainContainerEl.setAttribute("style", "display:block;"); // display quiz page
-    startTimer();
-    startQuestions();
-
-}
 
 // When button is clicked, start the timer
 function startTimer () {
@@ -56,35 +24,75 @@ function startTimer () {
         else {
             clearInterval(timeInterval);
             timerEl.innerHTML = "Time Left: 0"
+            // go to end screen
         }
         console.log(timeLeft)
     }, 1000)
 };
 
+// When button is clicked, start the quiz
+function startQuiz() {
+    
+    introContainerEl.setAttribute("style", "display:none;"); // hide start page
+    mainContainerEl.setAttribute("style", "display:block;"); // display quiz page
+    startTimer();
+    firstQuestion();
 
-function startQuestions() {
-
-    var questionEl = document.querySelector("#question");
-    questionEl.innerHTML = quizQuestions[0].question;
-
-    var answerButtonEl1 = document.querySelector("#option1");
-    answerButtonEl1.textContent = quizQuestions[0].possibleAnswers[0];
-
-    var answerButtonEl2 = document.querySelector("#option2");
-    answerButtonEl2.textContent  = quizQuestions[0].possibleAnswers[1];
-
-    var answerButtonEl3 = document.querySelector("#option3");
-    answerButtonEl3.textContent  = quizQuestions[0].possibleAnswers[2];
-
-    var answerButtonEl4 = document.querySelector("#option4");
-    answerButtonEl4.textContent = quizQuestions[0].possibleAnswers[3];
-
-    var answerContainer = document.querySelector("#answer-buttons");
-    answerContainer.addEventListener("click", nextQuestion);
 }
 
-function nextQuestion() {
+function firstQuestion() {
 
+    var questionEl = document.querySelector("#question");
+    questionEl.textContent = "What operator is used to assign a value to a declared variable?";
+
+    var answerButtonEl1 = document.querySelector("#option1");
+    answerButtonEl1.textContent = "a. Question mark (?)";
+
+    var answerButtonEl2 = document.querySelector("#option2");
+    answerButtonEl2.textContent  = "b. Colon (:)";
+
+    var answerButtonEl3 = document.querySelector("#option3");
+    answerButtonEl3.textContent = "c. Equal sign (=)";
+    answerButtonEl3.classList.add("correct")
+
+    var answerButtonEl4 = document.querySelector("#option4");
+    answerButtonEl4.textContent = "d. Double-equal (==)";
+
+    answerButtonEl.addEventListener("click", function(check) {
+        var answer = check.target;
+
+        if(answer.matches(".correct")){
+            resultEl.textContent = "Correct!"
+        }
+        else {
+            resultEl.textContent = "Wrong!"
+            //timeLeft -= 10
+        }
+        nextQuestion()
+    })
+
+
+}
+
+
+function nextQuestion() {
+    
+    var questionEl = document.querySelector("#question");
+    questionEl.textContent = "How do we declare a conditional statement in JavaScript?";
+
+    var answerButtonEl1 = document.querySelector("#option1");
+    answerButtonEl1.textContent = "a. while loop";
+
+    var answerButtonEl2 = document.querySelector("#option2");
+    answerButtonEl2.textContent  = "b. difference...between";
+
+    var answerButtonEl3 = document.querySelector("#option3");
+    answerButtonEl3.textContent = "c. for loop";
+    answerButtonEl3.classList.add("correct")
+
+    var answerButtonEl4 = document.querySelector("#option4");
+    answerButtonEl4.textContent = "d. if...else";
+    
 }
 
 
@@ -94,13 +102,6 @@ function nextQuestion() {
 // When question is answered incorrectly, subtract from the clock
 // When question is answered correctly, alert that question was right
 // When a question is answered, present another question
-
-function selectAnswer() {
-    if(givenAnswer === correctAnswer) {
-
-    }
-}
-
 // When all questions are answered OR the timer reaches 0, game is over
 
 // After last question, go to end screen
@@ -109,6 +110,10 @@ function endScreen() {
     introContainerEl.setAttribute("style", "display:none;"); // hide start page
     mainContainerEl.setAttribute("style", "display:none;"); // hide quiz page
     endContainerEl.setAttribute("style", "display:block;"); // show end page
+
+}
+
+function scoreForm() {
 
 }
 
